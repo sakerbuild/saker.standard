db02ede80296a97545b4b0aa93f31df5436011c4
sipkab <10866741+Sipkab@users.noreply.github.com>
2020-06-03 10:07:52 +0200

    Add Javadoc for SakerStandardTaskUtils

--- src/saker/std/api/util/SakerStandardTaskUtils.java
+++ src/saker/std/api/util/SakerStandardTaskUtils.java
@@ -1,8 +1,10 @@
 package saker.std.api.util;
 
 import saker.build.exception.InvalidPathFormatException;
+import saker.build.file.SakerFile;
 import saker.build.file.path.SakerPath;
 import saker.build.file.provider.SakerPathFiles;
+import saker.build.task.TaskContext;
 import saker.build.task.TaskFactory;
 import saker.build.task.identifier.TaskIdentifier;
 import saker.std.impl.file.mirror.MirroringTaskFactory;
@@ -17,12 +19,38 @@ public class SakerStandardTaskUtils {
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * Creates a task that performs {@linkplain TaskContext#mirror(SakerFile) mirroring} for the argument path.
+	 * <p>
+	 * The task should be started with the task identifier retrieved from
+	 * {@link #createMirroringTaskIdentifier(SakerPath)}.
+	 * 
+	 * @param executionPath
+	 *            The absolute execution path of the file or directory to mirror.
+	 * @return The task factory.
+	 * @throws NullPointerException
+	 *             If the argument is <code>null</code>.
+	 * @throws InvalidPathFormatException
+	 *             If the argument is not absolute.
+	 */
 	public static TaskFactory<? extends SakerPath> createMirroringTaskFactory(SakerPath executionPath)
 			throws NullPointerException, InvalidPathFormatException {
 		SakerPathFiles.requireAbsolutePath(executionPath);
 		return new MirroringTaskFactory(executionPath);
 	}
 
+	/**
+	 * Creates a task identifier for the {@linkplain #createMirroringTaskFactory(SakerPath) mirroring task}.
+	 * 
+	 * @param executionPath
+	 *            The absolute execution path of the file or directory to mirror. This should be the same as the path
+	 *            passed to {@link #createMirroringTaskFactory(SakerPath)}.
+	 * @return The task factory.
+	 * @throws NullPointerException
+	 *             If the argument is <code>null</code>.
+	 * @throws InvalidPathFormatException
+	 *             If the argument is not absolute.
+	 */
 	public static TaskIdentifier createMirroringTaskIdentifier(SakerPath executionPath)
 			throws NullPointerException, InvalidPathFormatException {
 		SakerPathFiles.requireAbsolutePath(executionPath);
